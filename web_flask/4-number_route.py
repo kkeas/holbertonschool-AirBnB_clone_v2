a9bcf4a4ac1a161577cf77a1fb2ba8e60cab42d9
kkeas <keaskatrina@gmail.com>
2023-03-27 14:01:27 -0700

    n is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""add a route"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def displayC(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python', defaults= {'text':'is cool'} ,methods=['GET'], strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
+def displayPython(text):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<n>', methods=['GET'], strict_slashes=False)
+def nisnumber(n):
+    if type(n) == int:
+        return "n is a number"
+    else:
+        return
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
