53121dbb6da4e5e0226bf7426b331ea93c8b4583
kkeas <keaskatrina@gmail.com>
2023-03-28 15:47:19 -0700

    pls daddy work

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,79 +1,61 @@
 #!/usr/bin/python3
-""""""
-from models.base_model import Base
 from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
-classes = {"Amenity": Amenity, "City": City, "Place": Place, "Review": Review,
-           "State": State, "User": User}
+from sqlalchemy import create_engine, inspect, MetaData
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base, BaseModel
+from models import city, place, review, state, amenity, user
 
 
 class DBStorage:
-    """"""
     __engine = None
     __session = None
+    CDIC = {
+        'City': city.City,
+        'Place': place.Place,
+        'Review': review.Review,
+        'State': state.State,
+        'Amenity': amenity.Amenity,
+        'User': user.User
+    }
 
     def __init__(self):
-        """"""
-        user = getenv("HBNB_MYSQL_USER")
-        pwd = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST")
-        db = getenv("HBNB_MYSQL_DB")
-        env = getenv("HBNB_ENV")
-
-        # Engine creation
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, pwd, host, db),
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+                                            getenv('HBNB_MYSQL_USER'),
+                                            getenv('HBNB_MYSQL_PWD'),
+                                            getenv('HBNB_MYSQL_HOST'),
+                                            getenv('HBNB_MYSQL_DB')),
                                       pool_pre_ping=True)
-        if env == "test":
-            Base.metadata.drop_all(self.__engine)
 
-    def all(self, cls=None):
-        """All method"""
-        my_dict = {}
-
-        if cls is None:
-            for my_type in classes.keys():
-                for obj in self.__session.query(classes[my_type]).all():
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    my_dict[key] = obj
-        else:
-            if isinstance(cls, str):
-                cls = classes[cls]
-            for obj in self.__session.query(cls).all():
-                key = obj.__class__.__name__ + '.' + obj.id
-                my_dict[key] = obj
-
-        return my_dict
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        the_session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(the_session)
+        self.__session = Session()
 
     def new(self, obj):
-        """"""
         self.__session.add(obj)
 
     def save(self):
-        """"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """"""
         if obj is not None:
             self.__session.delete(obj)
 
-    def reload(self):
-        """"""
-        Base.metadata.create_all(self.__engine)
-        newSession = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(newSession)
-        self.__session = Session
+    def all(self, cls=None):
+        obj_dct = {}
+        qry = []
+        if cls is None:
+            for cls_typ in DBStorage.CDIC.values():
+                qry.extend(self.__session.query(cls_typ).all())
+        else:
+            if cls in self.CDIC.keys():
+                cls = self.CDIC.get(cls)
+            qry = self.__session.query(cls)
+        for obj in qry:
+            obj_key = "{}.{}".format(type(obj).__name__, obj.id)
+            obj_dct[obj_key] = obj
+        return obj_dct
 
     def close(self):
-        """Closes scoped session"""
         self.__session.close()
