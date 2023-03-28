806204957ee621fae6d3afb977b00e6339adfce8
kkeas <keaskatrina@gmail.com>
2023-03-28 14:20:24 -0700

    pycodestyle

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,8 @@ def displayC(text):
     return "C " + text.replace("_", " ")
 
 
-@app.route('/python', defaults= {'text':'is cool'} ,methods=['GET'], strict_slashes=False)
+@app.route('/python', defaults={'text': 'is cool'}, methods=['GET'],
+           strict_slashes=False)
 @app.route('/python/<text>', methods=['GET'], strict_slashes=False)
 def displayPython(text):
     return "Python " + text.replace("_", " ")
